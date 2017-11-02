f16cfd2e5b48c793f742cfe59b195cbd274c1a08
Gabriel Ossers <gabrielossers@gmail.com>
2017-11-02 16:15:46 -0400

    Gabriel's first commit
    first commit

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+
+    $link = mysqli_connect("shareddb1c.hosting.stackcp.net", "mysecrets-3136b5ec", "QGFzqj2kT1Vu", "mysecrets-3136b5ec");
+        
+        if (mysqli_connect_error()) {
+            
+            die ("Database Connection Error");
+            
+        }
+
+?>
